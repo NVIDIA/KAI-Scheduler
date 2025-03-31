7b20bd84e679853ad7291e5bea03866a8aec2ebc
davidLif <davidshani12@gmail.com>
2025-03-31 14:05:31 +0300

    Add E2E k8s native integrations

---
+++ test/e2e/suites/integrations/k8s_native/k8s_native_suite_test.go
@@ -0,0 +1,16 @@
+package k8s_native
+
+import (
+	"testing"
+
+	"github.com/NVIDIA/KAI-scheduler/test/e2e/modules/utils"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+func TestK8sNativeIntegrations(t *testing.T) {
+	utils.SetLogger()
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "K8S Native Integrations Suite")
+}
