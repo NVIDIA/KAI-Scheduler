d1c67c3c527d2743b4c8530b72b48b6b443d6f95
davidLif <davidshani12@gmail.com>
2025-04-01 14:44:30 +0300

    Add ray helm path

--- hack/third_party_integrations/deploy_ray.sh
+++ hack/third_party_integrations/deploy_ray.sh
@@ -3,6 +3,6 @@
 # SPDX-License-Identifier: Apache-2.0
 set -e
 
-#helm repo add kuberay https://ray-project.github.io/kuberay-helm/
-#helm repo update
+helm repo add kuberay https://ray-project.github.io/kuberay-helm/
+helm repo update
 helm upgrade -i kuberay-operator --namespace ray --create-namespace kuberay/kuberay-operator --version 1.0.0
