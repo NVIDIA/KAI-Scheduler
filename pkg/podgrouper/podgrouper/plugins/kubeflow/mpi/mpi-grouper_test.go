d9e6fa8883531e6266751430b5f22a6d1d616dd7
davidLif <davidshani12@gmail.com>
2025-04-16 13:36:00 +0300

    Add mpi-grouper unitests

--- pkg/podgrouper/podgrouper/plugins/kubeflow/mpi/mpi-grouper_test.go
+++ pkg/podgrouper/podgrouper/plugins/kubeflow/mpi/mpi-grouper_test.go
@@ -1,3 +1,6 @@
+// Copyright 2025 NVIDIA CORPORATION
+// SPDX-License-Identifier: Apache-2.0
+
 package mpi
 
 import (
@@ -212,6 +215,184 @@ func TestHandleDelaidLauncherPolicy(t *testing.T) {
 			}
 			if !tt.expectedError && gp.MinAvailable != tt.expectedMinAvail {
 				t.Errorf("handleDelaidLauncherPolicy() MinAvailable = %v, want %v", gp.MinAvailable, tt.expectedMinAvail)
+			}
+		})
+	}
+}
+
+func TestGetPodGroupMetadata(t *testing.T) {
+	tests := []struct {
+		name             string
+		topOwner         *unstructured.Unstructured
+		pod              *v1.Pod
+		existingPods     []client.Object
+		expectedMinAvail int32
+		expectedError    bool
+	}{
+		{
+			name: "Basic pod group metadata",
+			topOwner: &unstructured.Unstructured{
+				Object: map[string]interface{}{
+					"metadata": map[string]interface{}{
+						"name":      "test-job",
+						"namespace": "test-ns",
+					},
+					"spec": map[string]interface{}{
+						ReplicaSpecName: map[string]interface{}{
+							MasterName: map[string]interface{}{
+								"replicas": int64(1),
+							},
+							WorkerName: map[string]interface{}{
+								"replicas": int64(2),
+							},
+						},
+					},
+				},
+			},
+			pod: &v1.Pod{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "worker-1",
+					Namespace: "test-ns",
+					Labels: map[string]string{
+						jobNameLabel:     "test-job",
+						podRoleNameLabel: "worker",
+					},
+				},
+			},
+			existingPods:     []client.Object{},
+			expectedMinAvail: 3, // 1 launcher + 2 workers
+			expectedError:    false,
+		},
+		{
+			name: "Delayed launcher policy with no launcher",
+			topOwner: &unstructured.Unstructured{
+				Object: map[string]interface{}{
+					"metadata": map[string]interface{}{
+						"name":      "test-job",
+						"namespace": "test-ns",
+					},
+					"spec": map[string]interface{}{
+						"launcherCreationPolicy": delaiedLauncherCreationPolicy,
+						ReplicaSpecName: map[string]interface{}{
+							MasterName: map[string]interface{}{
+								"replicas": int64(1),
+							},
+							WorkerName: map[string]interface{}{
+								"replicas": int64(2),
+							},
+						},
+					},
+				},
+			},
+			pod: &v1.Pod{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "worker-1",
+					Namespace: "test-ns",
+					Labels: map[string]string{
+						jobNameLabel:     "test-job",
+						podRoleNameLabel: "worker",
+					},
+				},
+			},
+			existingPods:     []client.Object{},
+			expectedMinAvail: 2, // Only workers since launcher is delayed
+			expectedError:    false,
+		},
+		{
+			name: "Delayed launcher policy with existing launcher",
+			topOwner: &unstructured.Unstructured{
+				Object: map[string]interface{}{
+					"metadata": map[string]interface{}{
+						"name":      "test-job",
+						"namespace": "test-ns",
+					},
+					"spec": map[string]interface{}{
+						"launcherCreationPolicy": delaiedLauncherCreationPolicy,
+						ReplicaSpecName: map[string]interface{}{
+							MasterName: map[string]interface{}{
+								"replicas": int64(1),
+							},
+							WorkerName: map[string]interface{}{
+								"replicas": int64(2),
+							},
+						},
+					},
+				},
+			},
+			pod: &v1.Pod{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "worker-1",
+					Namespace: "test-ns",
+					Labels: map[string]string{
+						jobNameLabel:     "test-job",
+						podRoleNameLabel: "worker",
+					},
+				},
+			},
+			existingPods: []client.Object{
+				&v1.Pod{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "launcher",
+						Namespace: "test-ns",
+						Labels: map[string]string{
+							jobNameLabel:     "test-job",
+							podRoleNameLabel: launcherPodRoleName,
+						},
+					},
+				},
+			},
+			expectedMinAvail: 3, // All pods including launcher
+			expectedError:    false,
+		},
+		{
+			name: "Invalid replicas configuration",
+			topOwner: &unstructured.Unstructured{
+				Object: map[string]interface{}{
+					"metadata": map[string]interface{}{
+						"name":      "test-job",
+						"namespace": "test-ns",
+					},
+					"spec": map[string]interface{}{
+						ReplicaSpecName: map[string]interface{}{
+							MasterName: map[string]interface{}{
+								"replicas": int64(0),
+							},
+						},
+					},
+				},
+			},
+			pod: &v1.Pod{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "worker-1",
+					Namespace: "test-ns",
+					Labels: map[string]string{
+						jobNameLabel:     "test-job",
+						podRoleNameLabel: "worker",
+					},
+				},
+			},
+			existingPods:     []client.Object{},
+			expectedMinAvail: 0,
+			expectedError:    true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			scheme := runtime.NewScheme()
+			_ = v1.AddToScheme(scheme)
+			fakeClient := fake.NewClientBuilder().WithScheme(scheme).WithObjects(tt.existingPods...).Build()
+
+			mg := NewMpiGrouper(fakeClient)
+			metadata, err := mg.GetPodGroupMetadata(tt.topOwner, tt.pod)
+
+			if (err != nil) != tt.expectedError {
+				t.Errorf("GetPodGroupMetadata() error = %v, expectedError %v", err, tt.expectedError)
+				return
+			}
+
+			if !tt.expectedError && metadata.MinAvailable != tt.expectedMinAvail {
+				t.Errorf("GetPodGroupMetadata() MinAvailable = %v, want %v", metadata.MinAvailable, tt.expectedMinAvail)
 			}
 		})
 	}
