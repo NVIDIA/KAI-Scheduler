97d80e5a32daf7e790191e09eaa78f8a720495df
Saurabh Kumar Singh <singh1203.ss@gmail.com>
2025-06-30 21:36:32 +0530

    refactor: Fix leader worker set grouper test
    Signed-off-by: Saurabh Kumar Singh <singh1203.ss@gmail.com>

--- pkg/podgrouper/podgrouper/plugins/leaderworkerset/lws_grouper_test.go
+++ pkg/podgrouper/podgrouper/plugins/leaderworkerset/lws_grouper_test.go
@@ -6,70 +6,113 @@ package leader_worker_set
 import (
 	"testing"
 
+	"github.com/NVIDIA/KAI-scheduler/pkg/podgrouper/podgrouper/plugins/defaultgrouper"
 	"github.com/stretchr/testify/assert"
 	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
-	"github.com/NVIDIA/KAI-scheduler/pkg/podgrouper/podgrouper/plugins/defaultgrouper"
 )
 
-func TestGetPodGroupMetadata_Parallel(t *testing.T) {
-	owner := &unstructured.Unstructured{
+func baseOwner(name string, startupPolicy string, replicas int64) *unstructured.Unstructured {
+	return &unstructured.Unstructured{
 		Object: map[string]interface{}{
 			"kind":       "LeaderWorkerSet",
-			"apiVersion": "lws.k8s.io/v1",
+			"apiVersion": "leaderworkerset.x-k8s.io/v1",
 			"metadata": map[string]interface{}{
-				"name":      "lws-test",
+				"name":      name,
 				"namespace": "default",
-				"uid":       "lws-uid",
+				"uid":       name + "-uid",
 			},
 			"spec": map[string]interface{}{
-				"startupPolicy": "Parallel",
+				"startupPolicy": startupPolicy,
 				"leaderWorkerTemplate": map[string]interface{}{
-					"replicas": int64(3),
+					"spec": replicas,
 				},
 			},
 		},
 	}
+}
 
-	pod := &v1.Pod{}
+func TestGetPodGroupMetadata_LeaderCreated(t *testing.T) {
+	owner := baseOwner("lws-test", "LeaderCreated", 3)
+
+	pod := &v1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Labels: map[string]string{},
+		},
+	}
 
 	lwsGrouper := NewLwsGrouper(defaultgrouper.NewDefaultGrouper("", ""))
 	podGroupMetadata, err := lwsGrouper.GetPodGroupMetadata(owner, pod)
 
 	assert.Nil(t, err)
-	assert.Equal(t, "LeaderWorkerSet", podGroupMetadata.Owner.Kind)
-	assert.Equal(t, "lws.k8s.io/v1", podGroupMetadata.Owner.APIVersion)
-	assert.Equal(t, "lws-uid", string(podGroupMetadata.Owner.UID))
-	assert.Equal(t, "lws-test", podGroupMetadata.Owner.Name)
 	assert.Equal(t, int32(3), podGroupMetadata.MinAvailable)
-	assert.Equal(t, "inference", podGroupMetadata.PriorityClassName)
+	assert.Equal(t, "LeaderWorkerSet", podGroupMetadata.Owner.Kind)
+	assert.Equal(t, "leaderworkerset.x-k8s.io/v1", podGroupMetadata.Owner.APIVersion)
+	assert.Equal(t, "lws-test", podGroupMetadata.Owner.Name)
+	assert.Equal(t, "lws-test-uid", string(podGroupMetadata.Owner.UID))
 }
 
-func TestGetPodGroupMetadata_Serial(t *testing.T) {
-	owner := &unstructured.Unstructured{
-		Object: map[string]interface{}{
-			"kind":       "LeaderWorkerSet",
-			"apiVersion": "lws.k8s.io/v1",
-			"metadata": map[string]interface{}{
-				"name":      "lws-test-serial",
-				"namespace": "default",
-				"uid":       "lws-serial-uid",
+func TestGetPodGroupMetadata_LeaderReady_LeaderPod(t *testing.T) {
+	owner := baseOwner("lws-ready", "LeaderReady", 5)
+
+	pod := &v1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Annotations: map[string]string{
+				"leaderworkerset.sigs.k8s.io/size": "5",
 			},
-			"spec": map[string]interface{}{
-				"startupPolicy": "Serial",
-				"leaderWorkerTemplate": map[string]interface{}{
-					"replicas": int64(5),
-				},
-			},
+			Labels: map[string]string{},
+		},
+		Spec: v1.PodSpec{
+			NodeName: "", // not scheduled => simulate leader
 		},
 	}
-
-	pod := &v1.Pod{}
 
 	lwsGrouper := NewLwsGrouper(defaultgrouper.NewDefaultGrouper("", ""))
 	podGroupMetadata, err := lwsGrouper.GetPodGroupMetadata(owner, pod)
 
 	assert.Nil(t, err)
 	assert.Equal(t, int32(1), podGroupMetadata.MinAvailable)
-	assert.Equal(t, "inference", podGroupMetadata.PriorityClassName)
+}
+
+func TestGetPodGroupMetadata_LeaderReady_WorkerPod(t *testing.T) {
+	owner := baseOwner("lws-ready", "LeaderReady", 5)
+
+	pod := &v1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Annotations: map[string]string{
+				"leaderworkerset.sigs.k8s.io/size": "5",
+			},
+			Labels: map[string]string{
+				"leaderworkerset.sigs.k8s.io/group-index": "0",
+			},
+		},
+		Spec: v1.PodSpec{
+			NodeName: "worker-node", // scheduled => simulate worker
+		},
+	}
+
+	lwsGrouper := NewLwsGrouper(defaultgrouper.NewDefaultGrouper("", ""))
+	podGroupMetadata, err := lwsGrouper.GetPodGroupMetadata(owner, pod)
+
+	assert.Nil(t, err)
+	assert.Equal(t, int32(5), podGroupMetadata.MinAvailable)
+}
+
+func TestGetPodGroupMetadata_GroupIndex_Label(t *testing.T) {
+	owner := baseOwner("lws-grouped", "LeaderCreated", 2)
+
+	pod := &v1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Labels: map[string]string{
+				"leaderworkerset.x-k8s.io/group-index": "1",
+			},
+		},
+	}
+
+	lwsGrouper := NewLwsGrouper(defaultgrouper.NewDefaultGrouper("", ""))
+	podGroupMetadata, err := lwsGrouper.GetPodGroupMetadata(owner, pod)
+
+	assert.Nil(t, err)
+	assert.Contains(t, podGroupMetadata.Name, "-group-1")
 }
