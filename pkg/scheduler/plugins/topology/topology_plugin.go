f101b3e766189a9ba9ca22d9b8b8389a5596b61b
davidLif <davidshani12@gmail.com>
2025-06-30 23:47:31 +0300

    Topology plugin - registration and tree construction

---
+++ pkg/scheduler/plugins/topology/topology_plugin.go
@@ -0,0 +1,126 @@
+// Copyright 2025 NVIDIA CORPORATION
+// SPDX-License-Identifier: Apache-2.0
+
+package topology
+
+import (
+	"github.com/NVIDIA/KAI-scheduler/pkg/scheduler/api/node_info"
+	"github.com/NVIDIA/KAI-scheduler/pkg/scheduler/api/pod_info"
+	"github.com/NVIDIA/KAI-scheduler/pkg/scheduler/framework"
+	"github.com/NVIDIA/KAI-scheduler/pkg/scheduler/log"
+	kueuev1alpha1 "sigs.k8s.io/kueue/apis/kueue/v1alpha1"
+)
+
+type topologyPlugin struct {
+	enabled       bool
+	TopologyTrees map[string]*TopologyInfo
+}
+
+func New(pluginArgs map[string]string) framework.Plugin {
+	return &topologyPlugin{
+		enabled:       true,
+		TopologyTrees: map[string]*TopologyInfo{},
+	}
+}
+
+func (t *topologyPlugin) Name() string {
+	return "topology"
+}
+
+func (t *topologyPlugin) OnSessionOpen(ssn *framework.Session) {
+	topologies, err := ssn.Cache.GetDataLister().ListTopologies()
+	if err != nil {
+		log.InfraLogger.Errorf("failed to list topologies", err)
+		return
+	}
+	t.initializeTopologyTree(topologies, ssn)
+
+	ssn.AddEventHandler(&framework.EventHandler{
+		AllocateFunc:   t.handleAllocate(ssn),
+		DeallocateFunc: t.handleDeallocate(ssn),
+	})
+}
+
+func (t *topologyPlugin) initializeTopologyTree(topologies []*kueuev1alpha1.Topology, ssn *framework.Session) {
+	for _, singleTopology := range topologies {
+		topologyTree := &TopologyInfo{
+			Name:             singleTopology.Name,
+			Domains:          map[TopologyDomainID]*TopologyDomainInfo{},
+			Root:             NewTopologyDomainInfo(TopologyDomainID("root"), "datacenter", "cluster", 0),
+			TopologyResource: singleTopology,
+		}
+
+		for _, nodeInfo := range ssn.Nodes {
+			t.addNodeDataDfsPath(topologyTree, singleTopology, nodeInfo)
+		}
+
+		t.TopologyTrees[singleTopology.Name] = topologyTree
+	}
+}
+
+func (*topologyPlugin) addNodeDataDfsPath(topologyTree *TopologyInfo, singleTopology *kueuev1alpha1.Topology, nodeInfo *node_info.NodeInfo) {
+	var previousDomainInfo *TopologyDomainInfo
+	for levelIndex := len(singleTopology.Spec.Levels) - 1; levelIndex >= 0; levelIndex-- {
+		level := singleTopology.Spec.Levels[levelIndex]
+
+		domainName, foundLevelLabel := nodeInfo.Node.Labels[level.NodeLabel]
+		if !foundLevelLabel {
+			continue // Skip if the node is not part of this level
+		}
+
+		domainId := calcDomainId(levelIndex, singleTopology.Spec.Levels, nodeInfo.Node.Labels)
+		domainInfo, foundLevelLabel := topologyTree.Domains[domainId]
+		if !foundLevelLabel {
+			domainInfo = NewTopologyDomainInfo(domainId, domainName, level.NodeLabel, levelIndex+1)
+			topologyTree.Domains[domainId] = domainInfo
+		}
+		domainInfo.AddNode(nodeInfo)
+
+		if previousDomainInfo != nil {
+			previousDomainInfo.Parent = domainInfo
+		}
+		previousDomainInfo = domainInfo
+	}
+	previousDomainInfo.Parent = topologyTree.Root
+}
+
+func (t *topologyPlugin) handleAllocate(ssn *framework.Session) func(event *framework.Event) {
+	return t.genericDfsEventHandler(ssn, func(domainInfo *TopologyDomainInfo, podInfo *pod_info.PodInfo) {
+		domainInfo.AllocatedResources.AddResourceRequirements(podInfo.AcceptedResource)
+		domainInfo.AllocatedPods = domainInfo.AllocatedPods + 1
+	})
+}
+
+func (t *topologyPlugin) handleDeallocate(ssn *framework.Session) func(event *framework.Event) {
+	return t.genericDfsEventHandler(ssn, func(domainInfo *TopologyDomainInfo, podInfo *pod_info.PodInfo) {
+		domainInfo.AllocatedResources.SubResourceRequirements(podInfo.AcceptedResource)
+		domainInfo.AllocatedPods = domainInfo.AllocatedPods - 1
+	})
+}
+
+func (t *topologyPlugin) genericDfsEventHandler(
+	ssn *framework.Session,
+	action func(domainInfo *TopologyDomainInfo, podInfo *pod_info.PodInfo),
+) func(event *framework.Event) {
+	return func(event *framework.Event) {
+		pod := event.Task.Pod
+		nodeName := event.Task.NodeName
+		node := ssn.Nodes[nodeName].Node
+		podInfo := ssn.Nodes[nodeName].PodInfos[pod_info.PodKey(pod)]
+
+		for _, topologyTree := range t.TopologyTrees {
+			leafDomainId := calcLeafDomainId(topologyTree.TopologyResource, node.Labels)
+			domainInfo := topologyTree.Domains[leafDomainId]
+			for domainInfo != nil {
+				action(domainInfo, podInfo)
+
+				if domainInfo.Nodes[nodeName] != nil {
+					break
+				}
+				domainInfo = domainInfo.Parent
+			}
+		}
+	}
+}
+
+func (t *topologyPlugin) OnSessionClose(ssn *framework.Session) {}
---
+++ pkg/scheduler/plugins/topology/topology_plugin_test.go
@@ -0,0 +1,213 @@
+// Copyright 2025 NVIDIA CORPORATION
+// SPDX-License-Identifier: Apache-2.0
+
+package topology
+
+import (
+	"context"
+	"testing"
+
+	kubeaischedulerver "github.com/NVIDIA/KAI-scheduler/pkg/apis/client/clientset/versioned/fake"
+	"github.com/NVIDIA/KAI-scheduler/pkg/common/constants"
+	"github.com/NVIDIA/KAI-scheduler/pkg/scheduler/api/node_info"
+	"github.com/NVIDIA/KAI-scheduler/pkg/scheduler/api/resource_info"
+	"github.com/NVIDIA/KAI-scheduler/pkg/scheduler/cache"
+	"github.com/NVIDIA/KAI-scheduler/pkg/scheduler/conf"
+	"github.com/NVIDIA/KAI-scheduler/pkg/scheduler/framework"
+	"github.com/stretchr/testify/assert"
+
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes/fake"
+
+	kueuev1alpha1 "sigs.k8s.io/kueue/apis/kueue/v1alpha1"
+	kueuefake "sigs.k8s.io/kueue/client-go/clientset/versioned/fake"
+)
+
+func TestTopologyPlugin(t *testing.T) {
+	fakeKubeClient := fake.NewSimpleClientset()
+	fakeKubeAISchedulerClient := kubeaischedulerver.NewSimpleClientset()
+	fakeKueueClient := kueuefake.NewSimpleClientset()
+
+	testNodes := []*v1.Node{
+		{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "test-node-0",
+				Labels: map[string]string{
+					"test-topology-label/block": "test-block-1",
+					"test-topology-label/rack":  "test-rack-1",
+				},
+			},
+			Spec: v1.NodeSpec{
+				Unschedulable: false,
+			},
+			Status: v1.NodeStatus{
+				Allocatable: v1.ResourceList{
+					"cpu":                 resource.MustParse("1"),
+					constants.GpuResource: resource.MustParse("1"),
+				},
+			},
+		},
+		{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "test-node-1",
+				Labels: map[string]string{
+					"test-topology-label/block": "test-block-1",
+					"test-topology-label/rack":  "test-rack-2",
+				},
+			},
+			Spec: v1.NodeSpec{
+				Unschedulable: false,
+			},
+			Status: v1.NodeStatus{
+				Allocatable: v1.ResourceList{
+					"cpu":                 resource.MustParse("1"),
+					constants.GpuResource: resource.MustParse("1"),
+				},
+			},
+		},
+		{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "test-node-2",
+				Labels: map[string]string{
+					"test-topology-label/block": "test-block-2",
+					"test-topology-label/rack":  "test-rack-1",
+				},
+			},
+			Spec: v1.NodeSpec{
+				Unschedulable: false,
+			},
+			Status: v1.NodeStatus{
+				Allocatable: v1.ResourceList{
+					"cpu":                 resource.MustParse("1"),
+					constants.GpuResource: resource.MustParse("3"),
+				},
+			},
+		},
+	}
+
+	testTopology := &kueuev1alpha1.Topology{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "test-topology",
+		},
+		Spec: kueuev1alpha1.TopologySpec{
+			Levels: []kueuev1alpha1.TopologyLevel{
+				{
+					NodeLabel: "test-topology-label/block",
+				},
+				{
+					NodeLabel: "test-topology-label/rack",
+				},
+			},
+		},
+	}
+
+	schedulerConfig := &conf.SchedulerConfiguration{
+		Actions: "allocate",
+		Tiers: []conf.Tier{
+			{
+				Plugins: []conf.PluginOption{
+					{
+						Name: "predicates",
+					},
+				},
+			},
+		},
+	}
+
+	schedulerParams := conf.SchedulerParams{
+		SchedulerName:   "test-scheduler",
+		PartitionParams: &conf.SchedulingNodePoolParams{},
+	}
+
+	schedulerCache := cache.New(&cache.SchedulerCacheParams{
+		KubeClient:                  fakeKubeClient,
+		KAISchedulerClient:          fakeKubeAISchedulerClient,
+		KueueClient:                 fakeKueueClient,
+		SchedulerName:               schedulerParams.SchedulerName,
+		NodePoolParams:              schedulerParams.PartitionParams,
+		RestrictNodeScheduling:      false,
+		DetailedFitErrors:           false,
+		ScheduleCSIStorage:          false,
+		FullHierarchyFairness:       true,
+		NodeLevelScheduler:          false,
+		NumOfStatusRecordingWorkers: 1,
+	})
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	var err error
+
+	for _, node := range testNodes {
+		_, err = fakeKubeClient.CoreV1().Nodes().Create(ctx, node, metav1.CreateOptions{})
+		assert.NoError(t, err)
+	}
+
+	_, err = fakeKueueClient.KueueV1alpha1().Topologies().Create(ctx, testTopology, metav1.CreateOptions{})
+	assert.NoError(t, err)
+
+	schedulerCache.Run(ctx.Done())
+	schedulerCache.WaitForCacheSync(ctx.Done())
+
+	session := &framework.Session{
+		Config:          schedulerConfig,
+		SchedulerParams: schedulerParams,
+		Cache:           schedulerCache,
+		Nodes: map[string]*node_info.NodeInfo{
+			testNodes[0].Name: {
+				Node:        testNodes[0],
+				Allocatable: resource_info.ResourceFromResourceList(testNodes[0].Status.Allocatable),
+				Used:        resource_info.NewResource(500, 0, 1),
+			},
+			testNodes[1].Name: {
+				Node:        testNodes[1],
+				Allocatable: resource_info.ResourceFromResourceList(testNodes[1].Status.Allocatable),
+				Used:        resource_info.NewResource(500, 0, 0),
+			},
+			testNodes[2].Name: {
+				Node:        testNodes[2],
+				Allocatable: resource_info.ResourceFromResourceList(testNodes[2].Status.Allocatable),
+				Used:        resource_info.NewResource(1, 0, 3),
+			},
+		},
+	}
+
+	plugin := New(nil)
+	plugin.OnSessionOpen(session)
+
+	topologyTrees := plugin.(*topologyPlugin).TopologyTrees
+	assert.Equal(t, 1, len(topologyTrees))
+	testTopologyObj := topologyTrees["test-topology"]
+	assert.Equal(t, "test-topology", testTopologyObj.Name)
+	assert.Equal(t, 5, len(testTopologyObj.Domains))
+
+	assert.Equal(t, "test-block-1", testTopologyObj.Domains["test-block-1"].Name)
+	assert.Equal(t, 1, testTopologyObj.Domains["test-block-1"].Depth)
+	assert.Equal(t, "CPU: 2 (cores), memory: 0 (GB), Gpus: 2",
+		testTopologyObj.Domains["test-block-1"].AvailableResources.String())
+
+	assert.Equal(t, "test-rack-1", testTopologyObj.Domains["test-block-1.test-rack-1"].Name)
+	assert.Equal(t, "test-block-1", testTopologyObj.Domains["test-block-1.test-rack-1"].Parent.Name)
+	assert.Equal(t, 2, testTopologyObj.Domains["test-block-1.test-rack-1"].Depth)
+	assert.Equal(t, "CPU: 1 (cores), memory: 0 (GB), Gpus: 1",
+		testTopologyObj.Domains["test-block-1.test-rack-1"].AvailableResources.String())
+
+	assert.Equal(t, "test-rack-2", testTopologyObj.Domains["test-block-1.test-rack-2"].Name)
+	assert.Equal(t, "test-block-1", testTopologyObj.Domains["test-block-1.test-rack-2"].Parent.Name)
+	assert.Equal(t, 2, testTopologyObj.Domains["test-block-1.test-rack-2"].Depth)
+	assert.Equal(t, "CPU: 1 (cores), memory: 0 (GB), Gpus: 1",
+		testTopologyObj.Domains["test-block-1.test-rack-2"].AvailableResources.String())
+
+	assert.Equal(t, "test-block-2", testTopologyObj.Domains["test-block-2"].Name)
+	assert.Equal(t, 1, testTopologyObj.Domains["test-block-2"].Depth)
+	assert.Equal(t, "CPU: 1 (cores), memory: 0 (GB), Gpus: 3",
+		testTopologyObj.Domains["test-block-2"].AvailableResources.String())
+
+	assert.Equal(t, "test-rack-1", testTopologyObj.Domains["test-block-2.test-rack-1"].Name)
+	assert.Equal(t, "test-block-2", testTopologyObj.Domains["test-block-2.test-rack-1"].Parent.Name)
+	assert.Equal(t, 2, testTopologyObj.Domains["test-block-2.test-rack-1"].Depth)
+	assert.Equal(t, "CPU: 1 (cores), memory: 0 (GB), Gpus: 3",
+		testTopologyObj.Domains["test-block-2.test-rack-1"].AvailableResources.String())
+}
