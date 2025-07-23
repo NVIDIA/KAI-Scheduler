6c2f7a8d766e07b72587d2da6fa86621dcc8164f
itsomri <omric@nvidia.com>
2025-07-23 15:45:04 +0300

    Added fair share simulator

---
+++ pkg/scheduler/plugins/proportion/resource_division/resource_division_simulator.go
@@ -0,0 +1,129 @@
+package resource_division
+
+import (
+	"time"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	consts "github.com/NVIDIA/KAI-scheduler/pkg/common/constants"
+	common_info "github.com/NVIDIA/KAI-scheduler/pkg/scheduler/api/common_info"
+	rs "github.com/NVIDIA/KAI-scheduler/pkg/scheduler/plugins/proportion/resource_share"
+	"knative.dev/pkg/ptr"
+)
+
+const (
+	defaultRequest = 100000
+)
+
+type ResourceShareOverrides struct {
+	Deserved                *float64 `json:"deserved"`
+	FairShare               *float64 `json:"fairShare"`
+	MaxAllowed              *float64 `json:"maxAllowed"`
+	OverQuotaWeight         *float64 `json:"overQuotaWeight"`
+	Allocated               *float64 `json:"allocated"`
+	AllocatedNotPreemptible *float64 `json:"allocatedNotPreemptible"`
+	Request                 *float64 `json:"request"`
+	AbsoluteUsage           *float64 `json:"absoluteUsage"`
+	VacantAdjustedUsage     *float64 `json:"vacantAdjustedUsage"`
+}
+
+func (r *ResourceShareOverrides) ResourceShare() *rs.ResourceShare {
+	rs := rs.ResourceShare{
+		Deserved:                0,
+		FairShare:               0,
+		MaxAllowed:              consts.UnlimitedResourceQuantity,
+		OverQuotaWeight:         1,
+		Allocated:               0,
+		AllocatedNotPreemptible: 0,
+		Request:                 defaultRequest,
+	}
+	if r.Deserved != nil {
+		rs.Deserved = *r.Deserved
+	}
+	if r.FairShare != nil {
+		rs.FairShare = *r.FairShare
+	}
+	if r.MaxAllowed != nil {
+		rs.MaxAllowed = *r.MaxAllowed
+	}
+	if r.OverQuotaWeight != nil {
+		rs.OverQuotaWeight = *r.OverQuotaWeight
+	}
+	if r.Allocated != nil {
+		rs.Allocated = *r.Allocated
+	}
+	if r.AllocatedNotPreemptible != nil {
+		rs.AllocatedNotPreemptible = *r.AllocatedNotPreemptible
+	}
+	if r.Request != nil {
+		rs.Request = *r.Request
+	}
+	return &rs
+}
+
+type QueueResourceShareOverrides struct {
+	GPU    *ResourceShareOverrides `json:"gpu"`
+	CPU    *ResourceShareOverrides `json:"cpu"`
+	Memory *ResourceShareOverrides `json:"memory"`
+}
+
+func (q QueueResourceShareOverrides) ResourceShare() rs.QueueResourceShare {
+	if q.GPU == nil {
+		q.GPU = &ResourceShareOverrides{}
+	}
+	if q.CPU == nil {
+		q.CPU = &ResourceShareOverrides{
+			Deserved: ptr.Float64(consts.UnlimitedResourceQuantity),
+		}
+	}
+	if q.Memory == nil {
+		q.Memory = &ResourceShareOverrides{
+			Deserved: ptr.Float64(consts.UnlimitedResourceQuantity),
+		}
+	}
+	rs := rs.QueueResourceShare{
+		GPU:    *q.GPU.ResourceShare(),
+		CPU:    *q.CPU.ResourceShare(),
+		Memory: *q.Memory.ResourceShare(),
+	}
+	return rs
+}
+
+type QueueOverrides struct {
+	UID               common_info.QueueID         `json:"uid"`
+	Name              string                      `json:"name"`
+	ParentQueue       common_info.QueueID         `json:"parentQueue"`
+	ChildQueues       []common_info.QueueID       `json:"childQueues"`
+	CreationTimestamp *string                     `json:"creationTimestamp"`
+	Priority          *int                        `json:"priority"`
+	ResourceShare     QueueResourceShareOverrides `json:"resourceShare"`
+}
+
+func (qo *QueueOverrides) ToQueueAttributes() *rs.QueueAttributes {
+	qa := &rs.QueueAttributes{
+		UID:                qo.UID,
+		Name:               qo.Name,
+		ParentQueue:        qo.ParentQueue,
+		ChildQueues:        qo.ChildQueues,
+		Priority:           0,
+		QueueResourceShare: qo.ResourceShare.ResourceShare(),
+	}
+
+	if qo.Priority != nil {
+		qa.Priority = *qo.Priority
+	}
+
+	if qo.CreationTimestamp != nil {
+		t, err := time.Parse(time.RFC3339, *qo.CreationTimestamp)
+		if err == nil {
+			qa.CreationTimestamp = metav1.Time{Time: t}
+		} else {
+			// handle error, perhaps log or default
+			qa.CreationTimestamp = metav1.Now()
+		}
+	} else {
+		qa.CreationTimestamp = metav1.Now()
+	}
+
+	return qa
+}
