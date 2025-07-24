5d472319ca42217d2c97c9ad6d1b8fad28e8308b
Roman Baron <roman.baron@run.ai>
2025-07-24 12:56:47 +0300

    Added SubGroups to PodGroup CRD and SubGroupInfo to PodGroupInfo

---
+++ pkg/scheduler/api/podgroup_info/subgroup_info.go
@@ -0,0 +1,31 @@
+// Copyright 2025 NVIDIA CORPORATION
+// SPDX-License-Identifier: Apache-2.0
+
+package podgroup_info
+
+import (
+	"github.com/NVIDIA/KAI-scheduler/pkg/apis/scheduling/v2alpha2"
+	"github.com/NVIDIA/KAI-scheduler/pkg/scheduler/api/pod_info"
+)
+
+type SubGroupInfo struct {
+	Name         string
+	MinAvailable int32
+	PodInfos     pod_info.PodsMap
+}
+
+func NewSubGroupInfo(name string, minAvailable int32) *SubGroupInfo {
+	return &SubGroupInfo{
+		Name:         name,
+		MinAvailable: minAvailable,
+		PodInfos:     pod_info.PodsMap{},
+	}
+}
+
+func FromSubGroup(subGroup *v2alpha2.SubGroup) *SubGroupInfo {
+	return NewSubGroupInfo(subGroup.Name, subGroup.MinMember)
+}
+
+func (sgi *SubGroupInfo) AddTaskInfo(ti *pod_info.PodInfo) {
+	sgi.PodInfos[ti.UID] = ti
+}
