91bceddfa9b5f308986fda4ee5fc61a55023fee4
Roman Baron <roman.baron@run.ai>
2025-08-07 13:18:22 +0300

    Fixed eviction_info tests

--- pkg/scheduler/api/podgroup_info/eviction_info_test.go
+++ pkg/scheduler/api/podgroup_info/eviction_info_test.go
@@ -56,18 +56,18 @@ func TestGetTasksToEvict_Table(t *testing.T) {
 		{
 			name: "WithSubGroups_SingleEvict",
 			job: func() *PodGroupInfo {
-				pg1 := NewPodGroupInfo("pg1")
-				pg1.MinAvailable = 2
-				pg1.SubGroups["sg1"] = NewSubGroupInfo("sg1", 1)
-				pg1.SubGroups["sg2"] = NewSubGroupInfo("sg2", 1)
+				pg := NewPodGroupInfo("pg1")
+				pg.MinAvailable = 2
+				pg.SubGroups["sg1"] = NewSubGroupInfo("sg1", 1)
+				pg.SubGroups["sg2"] = NewSubGroupInfo("sg2", 1)
 
-				pg1.AddTaskInfo(simpleTask("pod-1", "sg1", pod_status.Running))
-				pg1.AddTaskInfo(simpleTask("pod-2", "sg1", pod_status.Running))
-				pg1.AddTaskInfo(simpleTask("pod-3", "sg2", pod_status.Running))
-				return pg1
+				pg.AddTaskInfo(simpleTask("pod-1", "sg1", pod_status.Running))
+				pg.AddTaskInfo(simpleTask("pod-2", "sg1", pod_status.Running))
+				pg.AddTaskInfo(simpleTask("pod-3", "sg2", pod_status.Running))
+				return pg
 			}(),
 			tasksToEvict: true,
-			expectTasks:  []string{"pod-1"},
+			expectTasks:  []string{"pod-3"},
 		},
 		{
 			name: "WithSubGroups_EvictAll",
@@ -94,7 +94,7 @@ func TestGetTasksToEvict_Table(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			var tasks []*pod_info.PodInfo
-			tasks, tasksToEvict := GetTasksToEvict(tt.job, alwaysLess, alwaysLess)
+			tasks, tasksToEvict := GetTasksToEvict(tt.job, subGroupOrderFn, tasksOrderFn)
 			assert.Equal(t, tt.tasksToEvict, tasksToEvict)
 			var actualTaskNames []string
 			for _, pod := range tasks {
