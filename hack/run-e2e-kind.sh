553bb65aa53a804526ee4580e236f17768c5c5e4
itsomri <omric@nvidia.com>
2025-09-04 15:03:30 +0300

    Retry e2e

--- hack/run-e2e-kind.sh
+++ hack/run-e2e-kind.sh
@@ -44,47 +44,68 @@ while [[ $# -gt 0 ]]; do
   esac
 done
 
-kind create cluster --config ${KIND_CONFIG} --name $CLUSTER_NAME
-
-# Install the fake-gpu-operator to provide a fake GPU resources for the e2e tests
-helm upgrade -i gpu-operator oci://ghcr.io/run-ai/fake-gpu-operator/fake-gpu-operator --namespace gpu-operator --create-namespace --version 0.0.62 \
-    --values ${REPO_ROOT}/hack/fake-gpu-operator-values.yaml --wait
-
-# install third party operators to check the compatibility with the kai-scheduler
-if [ "$TEST_THIRD_PARTY_INTEGRATIONS" = "true" ]; then
-    ${REPO_ROOT}/hack/third_party_integrations/deploy_ray.sh
-    ${REPO_ROOT}/hack/third_party_integrations/deploy_kubeflow.sh
-    ${REPO_ROOT}/hack/third_party_integrations/deploy_knative.sh
-    ${REPO_ROOT}/hack/third_party_integrations/deploy_lws.sh
-fi
-
 if [ "$LOCAL_IMAGES_BUILD" = "true" ]; then
-    cd ${REPO_ROOT}
-    PACKAGE_VERSION=0.0.0-$(git rev-parse --short HEAD)
-    make build VERSION=$PACKAGE_VERSION
-    for image in $(docker images --format '{{.Repository}}:{{.Tag}}' | grep $PACKAGE_VERSION); do
-        kind load docker-image $image --name $CLUSTER_NAME
-    done
-    helm package ./deployments/kai-scheduler -d ./charts --app-version $PACKAGE_VERSION --version $PACKAGE_VERSION
-    helm upgrade -i kai-scheduler ./charts/kai-scheduler-$PACKAGE_VERSION.tgz  -n kai-scheduler --create-namespace --set "global.gpuSharing=true" --wait
-    rm -rf ./charts/kai-scheduler-$PACKAGE_VERSION.tgz 
-    cd ${REPO_ROOT}/hack
-else
-    PACKAGE_VERSION=0.0.0-$(git rev-parse --short origin/main)
-    helm upgrade -i kai-scheduler oci://ghcr.io/nvidia/kai-scheduler/kai-scheduler -n kai-scheduler --create-namespace --set "global.gpuSharing=true" --version "$PACKAGE_VERSION"
-fi
+      echo "OMRIC: Building local images"
+      cd ${REPO_ROOT}
+      PACKAGE_VERSION=0.0.0-$(git rev-parse --short HEAD)
+      make build VERSION=$PACKAGE_VERSION
+      
+      helm package ./deployments/kai-scheduler -d ./charts --app-version $PACKAGE_VERSION --version $PACKAGE_VERSION
+      
+      cd ${REPO_ROOT}/hack
+  else
+      PACKAGE_VERSION=0.0.0-$(git rev-parse --short origin/main)
+  fi
 
-# Allow all the pods in the fake-gpu-operator and kai-scheduler to start
-sleep 30
+for i in {1..100}; do
+  echo "Running attempt $i/100"
+  kind create cluster --config ${KIND_CONFIG} --name $CLUSTER_NAME
 
-# Install ginkgo if it's not installed
-if [ ! -f ${GOBIN}/ginkgo ]; then
-    echo "Installing ginkgo"
-    GOBIN=${GOBIN} go install github.com/onsi/ginkgo/v2/ginkgo@v2.23.3
-fi
+  # Install the fake-gpu-operator to provide a fake GPU resources for the e2e tests
+  helm upgrade -i gpu-operator oci://ghcr.io/run-ai/fake-gpu-operator/fake-gpu-operator --namespace gpu-operator --create-namespace --version 0.0.62 \
+      --values ${REPO_ROOT}/hack/fake-gpu-operator-values.yaml --wait
 
-${GOBIN}/ginkgo -r --keep-going --randomize-all --randomize-suites --label-filter '!autoscale && !scale' --trace -vv ${REPO_ROOT}/test/e2e/suites
+  # install third party operators to check the compatibility with the kai-scheduler
+  if [ "$TEST_THIRD_PARTY_INTEGRATIONS" = "true" ]; then
+      ${REPO_ROOT}/hack/third_party_integrations/deploy_ray.sh
+      ${REPO_ROOT}/hack/third_party_integrations/deploy_kubeflow.sh
+      ${REPO_ROOT}/hack/third_party_integrations/deploy_knative.sh
+      ${REPO_ROOT}/hack/third_party_integrations/deploy_lws.sh
+  fi
 
-if [ "$PRESERVE_CLUSTER" != "true" ]; then
-    kind delete cluster --name $CLUSTER_NAME
-fi
+  if [ "$LOCAL_IMAGES_BUILD" = "true" ]; then
+      echo "OMRIC: Loading local images"
+      cd ${REPO_ROOT}
+      PACKAGE_VERSION=0.0.0-$(git rev-parse --short HEAD)
+      for image in $(docker images --format '{{.Repository}}:{{.Tag}}' | grep $PACKAGE_VERSION); do
+          kind load docker-image $image --name $CLUSTER_NAME
+      done
+      ls
+      helm upgrade -i kai-scheduler ./charts/kai-scheduler-$PACKAGE_VERSION.tgz  -n kai-scheduler --create-namespace --set "global.gpuSharing=true" --wait
+      cd ${REPO_ROOT}/hack
+  else
+      PACKAGE_VERSION=0.0.0-$(git rev-parse --short origin/main)
+      helm upgrade -i kai-scheduler oci://ghcr.io/nvidia/kai-scheduler/kai-scheduler -n kai-scheduler --create-namespace --set "global.gpuSharing=true" --version "$PACKAGE_VERSION"
+  fi
+
+  # Allow all the pods in the fake-gpu-operator and kai-scheduler to start
+  sleep 30
+
+  # Install ginkgo if it's not installed
+  if [ ! -f ${GOBIN}/ginkgo ]; then
+      echo "Installing ginkgo"
+      GOBIN=${GOBIN} go install github.com/onsi/ginkgo/v2/ginkgo@v2.23.3
+  fi
+
+  if ! ${GOBIN}/ginkgo -r -focus "PodGroup Conditions Jobs Over Queue Limit NonPreemptible Job" --trace -vv ${REPO_ROOT}/test/e2e/suites/api/crds/podgroup; then
+      PRESERVE_CLUSTER="true"
+      kubectl logs -nkai-scheduler deploy/kai-scheduler > ~/flaky-podgroup-condition/kai-scheduler.log
+      kubectl logs -nkai-scheduler deploy/pod-grouper > ~/flaky-podgroup-condition/grouper.log
+      kubectl get podgroups.v2alpha2.scheduling.run.ai -A -oyaml > ~/flaky-podgroup-condition/podgroups.yaml
+      break
+  fi
+
+  if [ "$PRESERVE_CLUSTER" != "true" ]; then
+      kind delete cluster --name $CLUSTER_NAME
+  fi
+done
