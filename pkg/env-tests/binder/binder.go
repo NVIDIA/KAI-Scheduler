65828460c77ad23e89858747f0b9ca1387d596db
itsomri <omric@nvidia.com>
2025-09-17 10:55:27 +0300

    Added binder utils to env tests

---
+++ pkg/env-tests/binder/binder.go
@@ -0,0 +1,42 @@
+package binder
+
+import (
+	"k8s.io/client-go/rest"
+
+	"github.com/NVIDIA/KAI-scheduler/cmd/binder/app"
+	"github.com/NVIDIA/KAI-scheduler/pkg/binder/plugins"
+	"github.com/NVIDIA/KAI-scheduler/pkg/binder/plugins/gpusharing"
+	k8s_plugins "github.com/NVIDIA/KAI-scheduler/pkg/binder/plugins/k8s-plugins"
+)
+
+func RunBinder(cfg *rest.Config, stopCh chan struct{}) error {
+	options := app.InitOptions()
+	app, err := app.New(options, cfg)
+	if err != nil {
+		return err
+	}
+
+	err = registerPlugins(app)
+	if err != nil {
+		return err
+	}
+	go app.Run(stopCh)
+
+	return nil
+}
+
+func registerPlugins(app *app.App) error {
+	binderPlugins := plugins.New()
+	k8sPlugins, err := k8s_plugins.New(app.K8sInterface, app.InformerFactory,
+		int64(app.Options.VolumeBindingTimeoutSeconds))
+	if err != nil {
+		return err
+	}
+	binderPlugins.RegisterPlugin(k8sPlugins)
+
+	bindingGpuSharingPlugin := gpusharing.New(app.Client, app.Options.GpuCdiEnabled)
+
+	binderPlugins.RegisterPlugin(bindingGpuSharingPlugin)
+	app.RegisterPlugins(binderPlugins)
+	return nil
+}
