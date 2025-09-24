6b2ee0d7d59f62f75447df9dad0426ce40a3ac7a
Erez Freiberger <enoodle@gmail.com>
2025-09-24 16:24:58 +0200

    dra version aware client

---
+++ pkg/common/resources/dra_version_aware_client/client.go
@@ -0,0 +1,23 @@
+package draversionawareclient
+
+import (
+	"k8s.io/client-go/kubernetes"
+	resourcev1 "k8s.io/client-go/kubernetes/typed/resource/v1"
+	draclient "k8s.io/dynamic-resource-allocation/client"
+)
+
+type draAwareKubeClient struct {
+	kubernetes.Interface
+	draClient *draclient.Client
+}
+
+func NewDRAAwareClient(client kubernetes.Interface) kubernetes.Interface {
+	return &draAwareKubeClient{
+		Interface: client,
+		draClient: draclient.New(client),
+	}
+}
+
+func (c *draAwareKubeClient) ResourceV1() resourcev1.ResourceV1Interface {
+	return c.draClient
+}
