d109d6641d521fe05edb3e439e14ad6d353d9e15
Erez Freiberger <enoodle@gmail.com>
2025-09-25 16:46:05 +0200

    fix: setting DRA feature gate for scheduler

--- pkg/common/k8s_utils/features.go
+++ pkg/common/k8s_utils/features.go
@@ -5,15 +5,9 @@ package k8s_utils
 
 import (
 	"k8s.io/apiserver/pkg/util/feature"
-	"k8s.io/kubernetes/pkg/features"
 	k8splfeature "k8s.io/kubernetes/pkg/scheduler/framework/plugins/feature"
 )
 
 func GetK8sFeatures() k8splfeature.Features {
-	return k8splfeature.Features{
-		EnableDynamicResourceAllocation:    feature.DefaultFeatureGate.Enabled(features.DynamicResourceAllocation),
-		EnableDRAResourceClaimDeviceStatus: feature.DefaultFeatureGate.Enabled(features.DRAResourceClaimDeviceStatus),
-		EnableDRAExtendedResource:          feature.DefaultFeatureGate.Enabled(features.DRAExtendedResource),
-		EnableDRADeviceBindingConditions:   feature.DefaultFeatureGate.Enabled(features.DRADeviceBindingConditions),
-	}
+	return k8splfeature.NewSchedulerFeaturesFromGates(feature.DefaultMutableFeatureGate)
 }
