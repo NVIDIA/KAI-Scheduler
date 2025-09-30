110facd5f6f9901d0453bb3cec8cb68c0fe4c7e3
itsomri <omric@nvidia.com>
2025-09-30 10:56:18 +0300

    Removed dependency injection

--- pkg/common/podgroup/preemptible.go
+++ pkg/common/podgroup/preemptible.go
@@ -4,8 +4,6 @@
 package podgroup
 
 import (
-	"fmt"
-
 	"github.com/NVIDIA/KAI-scheduler/pkg/apis/scheduling/v2alpha2"
 )
 
@@ -13,21 +11,16 @@ const nonPreemptiblePriorityThreshold = 100
 
 // CalculatePreemptibility computes the preemptibility of a podgroup.
 // When preemptibility is not explicitly specified, the determination is based on the podgroup's priority.
-func CalculatePreemptibility(preemptibility v2alpha2.Preemptibility, getPriority func() (int32, error)) (v2alpha2.Preemptibility, error) {
+func CalculatePreemptibility(preemptibility v2alpha2.Preemptibility, priority int32) v2alpha2.Preemptibility {
 	switch preemptibility {
 	case v2alpha2.Preemptible:
-		return v2alpha2.Preemptible, nil
+		return v2alpha2.Preemptible
 	case v2alpha2.NonPreemptible:
-		return v2alpha2.NonPreemptible, nil
-	}
-
-	priority, err := getPriority()
-	if err != nil {
-		return "", fmt.Errorf("failed to get podgroup's priority: %w", err)
+		return v2alpha2.NonPreemptible
 	}
 
 	if priority < nonPreemptiblePriorityThreshold {
-		return v2alpha2.Preemptible, nil
+		return v2alpha2.Preemptible
 	}
-	return v2alpha2.NonPreemptible, nil
+	return v2alpha2.NonPreemptible
 }
