4e76c14c339ba141a87ec48bc11f6bf07e190701
itsomri <omric@nvidia.com>
2025-10-16 13:53:16 +0300

    Disable metrics and probes for env tests

--- pkg/env-tests/queuecontroller/queuecontroller.go
+++ pkg/env-tests/queuecontroller/queuecontroller.go
@@ -17,7 +17,7 @@ func RunQueueController(cfg *rest.Config, ctx context.Context) error {
 	opts := app.InitOptions(flag.NewFlagSet("", flag.ContinueOnError))
 
 	opts.EnableLeaderElection = false
-	opts.MetricsAddress = ":8084"
+	opts.MetricsAddress = "0"
 	opts.EnableWebhook = false
 	opts.SkipControllerNameValidation = true
 
