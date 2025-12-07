2ecdae41c18700f32f2045a4485c8823d0b22122
gshaibi <gshaibi@nvidia.com>
2025-12-07 23:19:14 +0200

    Update copyright information and SPDX license in topology_types.go

--- pkg/apis/kai/v1/topology_types.go
+++ pkg/apis/kai/v1/topology_types.go
@@ -14,6 +14,9 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+// Copyright 2025 NVIDIA CORPORATION
+// SPDX-License-Identifier: Apache-2.0
+
 package v1
 
 import (
