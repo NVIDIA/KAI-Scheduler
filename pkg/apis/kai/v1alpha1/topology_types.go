b70188b3761673e09938c6790cc734ee8292c861
gshaibi <gshaibi@nvidia.com>
2025-12-08 09:13:41 +0200

    start moving topology to v1alpha1

--- pkg/apis/kai/v1alpha1/topology_types.go
+++ pkg/apis/kai/v1alpha1/topology_types.go
@@ -17,7 +17,7 @@ limitations under the License.
 // Copyright 2025 NVIDIA CORPORATION
 // SPDX-License-Identifier: Apache-2.0
 
-package v1
+package v1alpha1
 
 import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
