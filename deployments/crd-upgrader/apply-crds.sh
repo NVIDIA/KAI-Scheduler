d06e359670576cb902035dbb1663f6658a753c11
gshaibi <gshaibi@nvidia.com>
2026-01-07 16:14:18 +0200

    fix: add migration for queues from v2alpha2

--- deployments/crd-upgrader/apply-crds.sh
+++ deployments/crd-upgrader/apply-crds.sh
@@ -2,6 +2,18 @@
 # Copyright 2025 NVIDIA CORPORATION
 # SPDX-License-Identifier: Apache-2.0
 
+migrate_queues_from_v2alpha2() {
+  if kubectl get crd queues.scheduling.run.ai &>/dev/null; then
+    if kubectl get crd queues.scheduling.run.ai -o jsonpath='{.status.storedVersions}' | grep -q 'v2alpha2'; then
+      echo "Migrating queues from v2alpha2"
+      kubectl get queues.scheduling.run.ai --all-namespaces -o json | kubectl replace -f -
+      # Remove the v2alpha2 version from the crd status.storedVersions
+      kubectl patch crd queues.scheduling.run.ai -p '{"status":{"storedVersions":[]}}' --subresource=status
+      echo "Queues migrated from v2alpha2"
+    fi
+  fi
+}
 
+migrate_queues_from_v2alpha2
 # Using --force-conflicts to claim ownership of the CRDs from helm
 kubectl apply --server-side=true --force-conflicts -f /internal-crds
